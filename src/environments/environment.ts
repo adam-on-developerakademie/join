bc5aad636d31c23d5ea106b70d5a4a49a036d0f3
adamp <adam.piskorek@gmx.de>
2025-10-21 00:34:56 +0200

    environment.firebaseConfig add for Adam

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: "db-join-461a1",
+    appId: "1:89985237003:web:9c653e42fc529623e5c70e",
+    storageBucket: "db-join-461a1.firebasestorage.app",
+    apiKey: "AIzaSyAWVotAOv_Bfb6oDFxG9ya0d8IU4uw2Sas",
+    authDomain: "db-join-461a1.firebaseapp.com",
+    messagingSenderId: "89985237003"
+  },
+};
