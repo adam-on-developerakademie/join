173fe2256e87802f045f528379220e389b345a36
Join DA <joinonda@gmail.com>
2025-10-20 22:36:28 +0200

    firebase added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "db-join-461a1", appId: "1:89985237003:web:9c653e42fc529623e5c70e", storageBucket: "db-join-461a1.firebasestorage.app", apiKey: "AIzaSyAWVotAOv_Bfb6oDFxG9ya0d8IU4uw2Sas", authDomain: "db-join-461a1.firebaseapp.com", messagingSenderId: "89985237003", projectNumber: "89985237003", version: "2" })), provideFirestore(() => getFirestore())
   ]
 };
