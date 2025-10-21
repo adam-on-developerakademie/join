a1d82ff31bf8102593160949480aca334ea38768
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-10-21 15:11:18 +0200

    fire join

---
+++ src/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAWotAov_Bfb6DoPKG9ya0d8tIU4uw2Sas",
+    authDomain: "db-join-461a1.firebaseapp.com",
+    projectId: "db-join-461a1",
+    storageBucket: "db-join-461a1.appspot.com",
+    messagingSenderId: "89985237003",
+    appId: "1:89985237003:web:9c653e42fc529623e5c70e"
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from './environment';
+
+bootstrapApplication(App, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    ...appConfig.providers,
+  ],
+}).catch(err => console.error(err));
