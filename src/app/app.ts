62d8261455fc22ee4dc65f50c4381e75d26647e2
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-10-23 12:24:36 +0200

    fix: warning contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header';
 import { SidenavComponent } from './shared/sidenav/sidenav';
 import { FbService } from './services/fb-service';
-import { Contacts } from './contacts/contacts';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Contacts, HeaderComponent, SidenavComponent],
+  imports: [RouterOutlet, HeaderComponent, SidenavComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
