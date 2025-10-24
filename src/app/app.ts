8f824fab4d2089a62673062084c0c325d71abfeb
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-10-25 01:43:09 +0200

    feat: fix : sidenav, header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/header/header';
-import { SidenavComponent } from './shared/sidenav/sidenav';
+//import { HeaderComponent } from './shared/header/header';
+//import { SidenavComponent } from './shared/sidenav/sidenav';
+import { FigmaHeader } from './shared/layout/figma-header/figma-header';
+import { FigmaSidenav } from './shared/layout/figma-sidenav/figma-sidenav';
+//
 import { FbService } from './services/fb-service';
 import { Contacts } from './contacts/contacts';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidenavComponent, Contacts],
+  imports: [RouterOutlet, FigmaHeader, FigmaSidenav, Contacts],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
