9b0de9c00f076960785685bc17eadbecdcb200f4
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-10-29 00:26:14 +0100

    fix: cancel, save button responsive

--- src/app/contacts/overlay-edit-contact/overlay-edit-contact.ts
+++ src/app/contacts/overlay-edit-contact/overlay-edit-contact.ts
@@ -2,56 +2,47 @@ import { Component } from '@angular/core';
 import { IContact } from '../../interfaces/i-contact';
 import { FbService } from '../../services/fb-service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-overlay-edit-contact',
-  imports: [FormsModule],
   standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './overlay-edit-contact.html',
   styleUrls: ['./overlay-edit-contact.scss'],
 })
 export class OverlayEditContactComponent {
-
-
-  constructor(private fbService: FbService) { this.getCurrentContact() }
+  constructor(private fbService: FbService) { this.getCurrentContact(); }
 
   contact: IContact = { name: '', surname: '', email: '', phone: '' };
-
   editedContact: IContact = { ...this.contact };
-
 
   onClose() {
     this.fbService.showEditContact = false;
   }
 
   delContact() {
-    this.fbService.contactsArray.length > 0 && this.fbService.contactsGroups.length > 0 &&
-      this.fbService.contactsArray.length > this.fbService.id ? this.fbService.delContact(this.fbService.id) : null;
+    const i = this.fbService.id ?? -1;
+    if (i >= 0 && i < this.fbService.contactsArray.length) {
+      this.fbService.delContact(i);
+    }
     this.fbService.showEditContact = false;
   }
 
-  getShowEditContact() {
-    return this.fbService.showEditContact;
-  }
-
-  upContact() {
+  async upContact() {
     if (!this.editedContact.name || !this.editedContact.surname || !this.editedContact.email) {
-      alert('Please fill in all required fields: Name, Surname, and Email.');
+      alert('Please fill in Name, Surname and Email.');
       return;
-    } else {
-      this.fbService.updateContact(this.fbService.id, this.editedContact);
-      this.fbService.showEditContact = false;
     }
+    const i = this.fbService.id ?? -1;
+    if (i >= 0 && i < this.fbService.contactsArray.length) {
+      await this.fbService.updateContact(i, this.editedContact);
+      this.fbService.setCurrentContact(i);
+    }
+    this.fbService.showEditContact = false;
   }
 
   getCurrentContact() {
     this.editedContact = { ...this.fbService.currentContact };
   }
-
-
-
-
-
 }
-
-
