b0c44fabca63e7274d7d476fdd350ffb9a409251
adamp <adam.piskorek@gmx.de>
2025-10-29 14:40:19 +0100

    back

--- src/app/contacts/overlay-add-contact/overlay-add-contact.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-overlay-add-contact',
-  standalone: true,
-  templateUrl: './overlay-add-contact.html',
-  styleUrls: ['./overlay-add-contact.scss'],
-})
-export class OverlayAddContactComponent {}
--- src/app/contacts/overlay-edit-contact-2/overlay-edit-contact-2.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-overlay-edit-contact-2',
-  standalone: true,
-  templateUrl: './overlay-edit-contact-2.html',
-  styleUrls: ['./overlay-edit-contact-2.scss'],
-})
-export class OverlayEditContact2Component {}
