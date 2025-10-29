b2fd41b7af47b642c8362fb46e6cae27b62921f0
WebZenSai <fabian.zeussel@gmx.de>
2025-10-29 01:47:25 +0100

    fix: contact slideIn

--- src/app/contacts/contact-card/contact-card.ts
+++ src/app/contacts/contact-card/contact-card.ts
@@ -3,22 +3,22 @@ import { CommonModule } from '@angular/common';
 import { IContact } from '../../interfaces/i-contact';
 import { FbService } from '../../services/fb-service';
 
-
 @Component({
   selector: 'app-contact-card',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './contact-card.html',
-  styleUrl: './contact-card.scss'
+  styleUrl: './contact-card.scss',
 })
 export class ContactCard {
+  slide = false;
+  currentContactId = -1;
   onEdit(): void {
     console.log('Edit contact clicked');
     // Implement edit functionality
   }
 
-  constructor(private fbService: FbService) { }
-
+  constructor(private fbService: FbService) {}
 
   onDelete(): void {
     console.log('Delete contact clicked');
@@ -30,14 +30,29 @@ export class ContactCard {
   }
 
   delContact() {
-    console.log(this.fbService.contactsGroups.length, this.fbService.id, this.fbService.addContact.length);
-    this.fbService.contactsArray.length > 0 && this.fbService.contactsGroups.length > 0 &&
-      this.fbService.contactsArray.length > this.fbService.id ? this.fbService.delContact(this.fbService.id) : null;
+    console.log(
+      this.fbService.contactsGroups.length,
+      this.fbService.id,
+      this.fbService.addContact.length
+    );
+    this.fbService.contactsArray.length > 0 &&
+    this.fbService.contactsGroups.length > 0 &&
+    this.fbService.contactsArray.length > this.fbService.id
+      ? this.fbService.delContact(this.fbService.id)
+      : null;
   }
-
 
   setEditContact() {
     this.fbService.showEditContact = true;
   }
-
+  setSlide() {
+    if (this.fbService.id != this.currentContactId) {
+      this.slide = true;
+      setTimeout(() => {
+        this.currentContactId = this.fbService.id;
+        this.slide = false;
+      }, 100);
+    }
+    return this.slide;
+  }
 }
